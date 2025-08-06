7e1cbd0f552ea32d8f55dcddc30301a824324cd6
whoisrahulmehta <mehta199928@gmail.com>
2025-08-06 10:26:31 +0530

    06-08-2025

--- src/app/api/connect/route.ts
+++ src/app/api/connect/route.ts
@@ -8,6 +8,20 @@ import { handleCors } from "@/_middlewares/options";
 import DailyCountModel from "@/_db/_models/counters/dailyCount";
 import lifeTimeCountModel from "@/_db/_models/counters/lifeTimeCount";
 
+/**
+ * Handles a POST request to connect the user.
+ *
+ * This function checks for CORS headers, validates the incoming API key
+ * from the request headers, and fetches the client signature from the
+ * request headers or cookies. If the key is valid, it creates or updates
+ * the user's daily and lifetime visits records in the DB. It then returns
+ * the user's visit numbers and signature in the response.
+ *
+ * @param {NextRequest} req - The incoming request object.
+ *
+ * @returns {Promise<NextResponse>} The response containing the user's visit
+ * numbers and signature, or an error message if the operation fails.
+ */
 export async function POST(req: NextRequest) {
   const corsHeaders = await handleCors(req);
   if (corsHeaders instanceof NextResponse) return corsHeaders;
